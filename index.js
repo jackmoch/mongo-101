aba6ed6785bcb0f25439f5f84ebfd583717c7128
Jack Mocherman <mochermanjack@gmail.com>
2016-09-13 09:44:09 -0500

    changed connect format to a promise

--- index.js
+++ index.js
@@ -3,7 +3,9 @@
 const { MongoClient } = require('mongodb')
 const MONGODB_URL = 'mongodb://localhost:27017/test'
 
-MongoClient.connect(MONGODB_URL, (err, db) => {
+MongoClient
+.connect(MONGODB_URL)
+.then(db => {
 	console.log(db)
 	db.close()
 })
