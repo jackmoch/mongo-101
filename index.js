b7ce5cc92e52c027a505a12cc19f12738e539dcb
Jack Mocherman <mochermanjack@gmail.com>
2016-09-13 09:58:59 -0500

    retrieved data from restaurants db, converted to array, and then console logged, close command must be in then statement of toArray function or connection will close before consol log fires

--- index.js
+++ index.js
@@ -6,8 +6,15 @@ const MONGODB_URL = 'mongodb://localhost:27017/test'
 MongoClient
 	.connect(MONGODB_URL)
 	.then(db => {
-		console.log(db)
-		db.close()
+		db.collection('restaurants')
+			.find({})
+			.toArray()
+			.then(data => {
+				console.log(data)
+				db.close()
+			})
+			.catch(console.error)
+
 	})
 	.catch(console.error)
 
